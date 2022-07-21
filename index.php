3529e9ec1cf8caea37a4be81e9d3afbf79b78eda
nur-adila <109575975+nur-adila@users.noreply.github.com>
2022-07-21 17:02:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Warung</title>
+	<meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="shortcut icon" href="images/favicon.png" type="">
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+<body>
+ <section class="food_section layout_padding-bottom">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Contoh Menu
+        </h2>
+      </div>
+      <ul class="filters_menu">
+        <li class="active" data-filter="*">All</li>
+        <li data-filter=".nasigoreng">Nasi Goreng</li>
+        <li data-filter=".ayam">Ayam</li>
+        <li data-filter=".bakso">Bakso</li>
+        <li data-filter=".minuman">Minuman</li>
+      </ul>
+      <div class="filters-content">
+        <div class="row grid">
+          <div class="col-sm-6 col-lg-4 all ayam">
+            <div class="box">
+              <div>
+                <div class="img-box">
+                  <img src="gambar/Ayam Rendang.jpg" alt="">
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    Ayam Rendang
+                  </h5>
+                  <p>
+                    Keunikan rendang adalah penggunaan bumbu-bumbu alami, yang bersifat antiseptik dan membunuh bakteri patogen sehingga bersifat  pengawet alami. 
+                  </p>
+                  <div class="options">
+                    <h6>
+                      Rp. 25.000
+                    </h6>
+                    <a href="warung-onlen/login.php">
+                      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 all nasigoreng">
+            <div class="box">
+              <div>
+                <div class="img-box">
+                  <img src="gambar/Nasi Goreng.jpg" alt="">
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    Nasi Goreng Biasa
+                  </h5>
+                  <p>
+                    Keistimewaan nasi goreng adalah campuran bumbu yang mempengaruhi rasa. Sebab itulah ada beragam nasi goreng di Indonesia
+                  </p>
+                  <div class="options">
+                    <h6>
+                      Rp. 13.000
+                    </h6>
+                    <a href="warung-onlen/login.php">
+                      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 all ayam">
+            <div class="box">
+              <div>
+                <div class="img-box">
+                  <img src="gambar/ayam-geprek.jpeg" alt="">
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    Ayam Geprek
+                  </h5>
+                  <p>
+                    Geprek adalah istilah dalam bahasa Jawa yang berarti "dipukul", "ditekan", atau "dilumatkan", maka ayam geprek berarti "ayam yang dipukul"
+                  </p>
+                  <div class="options">
+                    <h6>
+                      Rp. 12.000
+                    </h6>
+                    <a href="warung-onlen/login.php">
+                      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+</body>
+</html>
